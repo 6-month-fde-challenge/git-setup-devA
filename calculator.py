4cfe1d3478943eaa309b982b3e8d167ad5c105ad
Veerandra Kumar <potnuru.veerandra@gmail.com>
2026-09-21 15:34:23 +0530

    made hot fixes

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 from input_variables import a ,b
 from subtract_module import subtract
 from multiply_module import multuply
-
+from addition_module import addition
 
 subtraction = subtract(a,b)
 multiplication = multuply(a,b)
+sum = addition(a,b)
 
-print("addition of the both of the products is : " , subtraction + multiplication)
+print("addition of two number is  : " , sum)
+print("addition of the both of the results of subtration and multiplication is : " , subtraction + multiplication)
+
