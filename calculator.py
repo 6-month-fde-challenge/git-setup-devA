8d13dd97026558cbe6d3bb9f3a8efe0502d5de62
Veerandra Kumar <potnuru.veerandra@gmail.com>
2026-09-21 15:39:24 +0530

    commit2

--- calculator.py
+++ calculator.py
@@ -2,11 +2,14 @@ from input_variables import a ,b
 from subtract_module import subtract
 from multiply_module import multuply
 from addition_module import addition
+from division_module import division
 
 subtraction = subtract(a,b)
 multiplication = multuply(a,b)
 sum = addition(a,b)
+div = division(a,b)
 
 print("addition of two number is  : " , sum)
+print("Division of two number is  : " , div)
 print("addition of the both of the results of subtration and multiplication is : " , subtraction + multiplication)
 
